4ecbadffeac0695cc910653f1ba1216843aefd13
Nikita Gusakov <gusnkt@gmail.com>
2017-07-17 18:02:12 +0300

    feat: add entry point

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2017 dialog LLC <info@dlg.im>
+ * @flow
+ */
+
+// Interface
+export { default as Icon } from './components/Icon/Icon';
+export { default as Avatar } from './components/Avatar/Avatar';
+
+// Discover
+export { default as Discover } from './components/Discover/Discover';
+export { default as DiscoverCard } from './components/DiscoverCard/DiscoverCard';

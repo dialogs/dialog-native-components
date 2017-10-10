af117074d6092011b6b648155ea027dc03af99d0
Nikita Gusakov <gusnkt@gmail.com>
2017-10-10 15:41:30 +0300

    feat: export new components

--- src/index.js
+++ src/index.js
@@ -11,5 +11,14 @@ export { default as Avatar } from './components/Avatar/Avatar';
 export { default as Discover } from './components/Discover/Discover';
 export { default as DiscoverCard } from './components/DiscoverCard/DiscoverCard';
 
+// Sights
+export { default as Sights } from './components/Sights/Sights';
+export { default as SightsItem } from './components/SightsItem/SightsItem';
+
+// Schedule
+export { default as Schedule } from './components/Schedule/Schedule';
+export { default as ScheduleDay } from './components/ScheduleDay/ScheduleDay';
+export { default as ScheduleEvent } from './components/ScheduleEvent/ScheduleEvent';
+
 // Utils
 export { default as ContextProvider } from './components/ContextProvider/ContextProvider';

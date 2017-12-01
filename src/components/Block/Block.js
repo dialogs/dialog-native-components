57deca996a637ee65a06b4a1fe311ef3ac2336ad
Oleg Shilov <me@olegshilov.com>
2017-12-01 23:17:27 +0300

    feat(Block): add simple styled block component

---
+++ src/components/Block/Block.js
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2017 dialog LLC <info@dlg.im>
+ * @flow
+ */
+
+import PropTypes from 'prop-types';
+import React, { PureComponent } from 'react';
+import { View } from 'react-native';
+import getStyles from './styles';
+
+type Props = {
+  title?: ?string,
+  style: any,
+  children: any
+};
+
+class Block extends PureComponent {
+  props: Props;
+  styles: Object;
+
+  static contextTypes = {
+    theme: PropTypes.object,
+    style: PropTypes.object,
+    locale: PropTypes.string
+  };
+
+  constructor(props, context) {
+    super(props, context);
+
+    this.styles = getStyles(context.theme, context.style.Block);
+  }
+
+  renderTitle() {
+    return null;
+  }
+
+  render() {
+    return (
+      <View style={[this.styles.container, this.props.style]}>
+        {this.renderTitle()}
+        {this.props.children}
+      </View>
+    );
+  }
+}
+
+export default Block;
---
+++ src/components/Block/styles.js
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2017 dialog LLC <info@dlg.im>
+ * @flow
+ */
+
+import { StyleSheet } from 'react-native';
+import { Color, Padding } from '../../styles';
+
+const getStyles = (theme, styleOverride) => {
+  const style = {
+    container: {
+      elevation: 2,
+      backgroundColor: theme.color.white || Color.white,
+      marginBottom: Padding.default * 2
+    },
+    title: {
+      color: theme.color.primary || Color.primary,
+      fontSize: 14
+    }
+  };
+
+  return StyleSheet.create(Object.assign({}, style, styleOverride));
+};
+
+export default getStyles;

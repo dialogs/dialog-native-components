2a70d339cfe9df126dc953b8d2da32d9f87ba4fa
Oleg Shilov <me@olegshilov.com>
2017-12-01 23:13:00 +0300

    feat(Icon): add new icons
    set icon default size

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -16,13 +16,16 @@ type Props = {
 class Icon extends PureComponent {
   props: Props;
 
+  static defaultProps = {
+    size: 32
+  };
+
   static contextTypes = {
     icons: PropTypes.object
   };
 
   getImage = () => {
     const { glyph } = this.props;
-
     if (this.context.icons && this.context.icons[glyph]) {
       return { uri: this.context.icons[glyph], cache: 'force-cache' };
     } else if (Platform.OS === 'ios') {
@@ -30,28 +33,40 @@ class Icon extends PureComponent {
     }
 
     switch (glyph) {
-      case 'person':
-        return require('../../assets/icons/person.png');
-      case 'group':
-        return require('../../assets/icons/group.png');
+      case 'arrow':
+        return require('../../assets/icons/arrow.png');
+      case 'arrow_right':
+        return require('../../assets/icons/arrow_right.png');
+      case 'block':
+        return require('../../assets/icons/block.png');
+      case 'bug':
+        return require('../../assets/icons/bug.png');
+      case 'call':
+        return require('../../assets/icons/call.png');
       case 'channel':
         return require('../../assets/icons/channel.png');
       case 'error':
         return require('../../assets/icons/error.png');
-      case 'block':
-        return require('../../assets/icons/block.png');
+      case 'group':
+        return require('../../assets/icons/group.png');
       case 'list':
         return require('../../assets/icons/list.png');
       case 'logo':
         return require('../../assets/icons/logo.png');
+      case 'marker':
+        return require('../../assets/icons/marker.png');
+      case 'notification':
+        return require('../../assets/icons/notification.png');
+      case 'person':
+        return require('../../assets/icons/person.png');
+      case 'question':
+        return require('../../assets/icons/question.png');
+      case 'security':
+        return require('../../assets/icons/security.png');
       case 'star':
         return require('../../assets/icons/star.png');
       case 'star_outline':
         return require('../../assets/icons/star_outline.png');
-      case 'call':
-        return require('../../assets/icons/call.png');
-      case 'notification':
-        return require('../../assets/icons/notification.png');
       default:
         return null;
     }

53e776996550e170b5e7e6c47abc01f3ebc817b7
Nikita Gusakov <gusnkt@gmail.com>
2018-01-30 16:33:32 +0300

    wip

--- src/previews/index.js
+++ src/previews/index.js
@@ -12,6 +12,7 @@ import ContactsPreview from './ContactsPreview';
 import ProfilePreview from './ProfilePreview';
 import UserProfilePreview from './UserProfilePreview';
 import DialpadPreview from './DialpadPreview';
+import messages from '../messages';
 
 const theme = {
   color: {
@@ -23,62 +24,6 @@ const theme = {
 };
 const style = {};
 const locale = 'ru';
-const messages = {
-  ru: {
-    'Button.get_direction': 'Проложить маршрут',
-    'Button.open_chat': 'Открыть чат',
-    'Error.nothing_found': 'Ничего не найдено',
-    'Yes': 'Да',
-    'No': 'Нет',
-    'Profile.button_call': 'Позвонить',
-    'Profile.button_message': 'Написать',
-    'Profile.about': 'О себе',
-    'Profile.about_add_button': 'Добавить описание',
-    'Profile.nick': 'Никнейм',
-    'Profile.nick_add_button': 'Выберите никнейм',
-    'Profile.nick_hint':
-      'Вы можете выбрать никнейм, чтобы другие люди могли найти вас, не зная номер вашего телефона.',
-    'Profile.phone': 'Телефон',
-    'Profile.email': 'E-mail',
-    'Profile.notifications': 'Уведомления',
-    'Profile.media': 'Общие медиа',
-    'Profile.favourite_enable': 'Удалить из избранного',
-    'Profile.favourite_disable': 'Добавить в избранное',
-    'Profile.block': 'Заблокировать пользователя',
-    'User.notifications': 'Уведомления и звуки',
-    'User.privacy': 'Конфиденциальность и безопасность',
-    'User.advanced_settings': 'Расширенные настройки',
-    'User.ask_a_question': 'Задать вопрос в чате',
-    'User.feedback': 'Отправить отзыв'
-  },
-  en: {
-    'Button.get_direction': 'Get directions',
-    'Button.open_chat': 'Open chat',
-    'Error.nothing_found': 'Nothing found',
-    'Yes': 'Yes',
-    'No': 'No',
-    'Profile.button_call': 'Call',
-    'Profile.button_message': 'Message',
-    'Profile.about': 'About',
-    'Profile.about_add_button': 'Describe yourself',
-    'Profile.nick': 'Nickname',
-    'Profile.nick_add_button': 'Choose a nickname',
-    'Profile.nick_hint':
-      'You can choose a nickname, so other people will be able to find you by this nickname and contact you without knowing your phone number.',
-    'Profile.phone': 'Phone',
-    'Profile.email': 'E-mail',
-    'Profile.notifications': 'Notifications',
-    'Profile.media': 'Shared media',
-    'Profile.favourite_enable': 'Remove from favourites',
-    'Profile.favourite_disable': 'Add to favourites',
-    'Profile.block': 'Block user',
-    'User.notifications': 'Notifications and sounds',
-    'User.privacy': 'Privacy and security',
-    'User.advanced_settings': 'Advanced settings',
-    'User.ask_a_question': 'Ask question in chat',
-    'User.feedback': 'Submit feedback'
-  }
-};
 const globalValues = {};
 
 const tabs = {

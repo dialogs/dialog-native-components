37078d3ca19337db2c2409cfd58a52402dc7261d
Oleg Shilov <me@olegshilov.com>
2018-03-26 15:03:07 +0300

    chore: import more components

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@
  * @flow
  */
 
+// Utils
+export { default as ContextProvider } from './components/ContextProvider/ContextProvider';
+
 // Interface
 export { default as Icon } from './components/Icon/Icon';
 export { default as Avatar } from './components/Avatar/Avatar';
+export { default as Map } from './components/Map/Map';
+export { default as Button } from './components/Button/Button';
 
 // Discover
 export { default as Discover } from './components/Discover/Discover';
@@ -23,9 +28,6 @@ export { default as ContactsItem } from './components/ContactsItem/ContactsItem'
 export { default as Schedule } from './components/Schedule/Schedule';
 export { default as ScheduleDay } from './components/ScheduleDay/ScheduleDay';
 export { default as ScheduleEvent } from './components/ScheduleEvent/ScheduleEvent';
-
-// Utils
-export { default as ContextProvider } from './components/ContextProvider/ContextProvider';
 
 // Profile
 export { default as Profile } from './components/Profile/Profile';
